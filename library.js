33032d5eb9c8cde7f057ff587e6790916adcb5a6
Rakib Muhammed Saki <rakib1saki@gmail.com>
2022-05-08 13:37:12 +0600

    Start drawing

--- library.js
+++ library.js
@@ -1,17 +1,34 @@
-class Measurement {
-    constructor(of, value) {
-        this.of = of
-        this.value = value
-        this.lines = []
+class মাপ {
+    constructor(অংশ, মান) {
+        this.অংশ = অংশ
+        this.মান = মান
+        this.মাপসমূহ = []
     }
 }
 
-class Line {
+class দাগ {
     // from can be coordinates or (function to) point on another line
-    // to can be relative polar coordinates or (function to) point on another line
-    constructor(from, to, curveAbout) {
-        this.from = from
-        this.to = to
-        this.curveAbout = curveAbout
+    // to can be relative polar coordinates or (function to) point on another line or (function to) point according to measurement
+    constructor(শুরু, শেষ, ঘুরবে) {
+        this.শুরু = শুরু
+        this.শেষ = শেষ
+        this.ঘুরবে = ঘুরবে
+    }
+    আঁকো() {
+        let শুরু = this.শুরু
+        if (typeof this.শুরু == 'function') {
+            শুরু = this.শুরু()
+        }
+        let শেষ = this.শেষ
+        if (typeof this.শেষ == 'function') {
+            শেষ = this.শেষ()
+        } else {
+            // add polar coordinate to start
+            শেষ = [শুরু[0] + (শেষ[0] * Math.cos(শেষ[1])), শুরু[1] + (শেষ[0] * Math.sin(শেষ[1]))]
+        }
+        ঘুরবে = this.ঘুরবে(শুরু, শেষ)
+        beginShape(শুরু[0], শুরু[1])
+        quadraticVertex(ঘুরবে[0], ঘুরবে[শেষ], শেষ[0], শেষ[1])
+        endShape()
     }
 }
---
+++ script.js
@@ -0,0 +1,8 @@
+setup = () => {
+    createCanvas(innerWidth, innerHeight)
+}
+
+draw = () => {
+    background(255)
+    stroke(0)
+}
